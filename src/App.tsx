ed3168afd1372d924ad5c4b7642b33b1b205b3ba
Jonas Nunes <jonasctps@gmail.com>
2022-07-27 16:12:10 -0300

    Add variaveis no global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/header";
+import { GlobalStyles } from "./styles/global";
 
 export function App() {
   return (
     <div>
       <Header />
+      <GlobalStyles />
     </div>
   )
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,1 +1,24 @@
-import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+
+:root{
+  --background: #FFFFFF;
+  --text: #112B3C;
+  --sub-text: #828282;
+  --orange: #F66B0E;
+  --light-gray: #BDBDBD;
+
+}
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html{
+  font-size: 62.5%;
+  overflow-x: hidden;
+}
+`

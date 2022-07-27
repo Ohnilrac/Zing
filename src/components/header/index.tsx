f20f7e33f75f37f97e660b407e735b37455b2909
Jonas Nunes <jonasctps@gmail.com>
2022-07-27 00:03:29 -0300

    Criando componente header e add styled components

---
+++ src/components/header/index.tsx
@@ -0,0 +1,23 @@
+export function Header(){
+  return (
+    <header>
+      <nav>
+        <h2>Zing<span>.</span></h2>
+        <div>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Products</li>
+            <li>Testimonials</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Contact</li>
+            <li>Buy online</li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+    )
+}

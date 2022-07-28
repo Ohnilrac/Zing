857410f848d25167eacdc937933e423576950aa1
Jonas Nunes <jonasctps@gmail.com>
2022-07-28 11:54:48 -0300

    Home 1.1
    estilizacao do home

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,16 +1,20 @@
-import { Container } from "./styles";
+import { Container, Content } from "./styles";
+import { AiOutlineSearch } from 'react-icons/ai'
+
 import bgHome from "../../assets/bg-home.png"
 
 export function Home(){
   return (
     <Container>
-      <h1>Móveis incríveis para sua casa</h1>
-      <p>Acreditamos que a cadeira é o móvel mais importante da sua casa. Porque se você ama a cadeira em que está sentado, é provável que você ame o resto da sala.</p>
-      <div>
-        <input type="text" />
-        <i></i>
-      </div>
-      <img src={bgHome} alt="Uma sala com alguns jarros de plantas, uma poltrona laranja e um apoio para as pernas" />
+      <Content>
+        <h1>Móveis incríveis para sua casa</h1>
+        <p>Acreditamos que a cadeira é o móvel mais importante da sua casa. Porque se você ama a cadeira em que está sentado, é provável que você ame o resto da sala.</p>
+        <div>
+          <input type="text" placeholder="Procurar por produtos" />
+        </div>
+          <i> <AiOutlineSearch /> </i>
+        <img src={bgHome} alt="Uma sala com alguns jarros de plantas, uma poltrona laranja e um apoio para as pernas" />
+      </Content>
     </Container>
   )
 }
--- src/components/home/styles.ts
+++ src/components/home/styles.ts
@@ -4,3 +4,50 @@ export const Container = styled.main`
   width: 80%;
   margin: 0 auto;
 `
+
+export const Content = styled.div`
+  width: 100%;
+  margin-top: 6rem;
+  text-align: center;
+
+  h1{
+    font-size: 4rem;
+    font-weight: 700;
+    color: var(--text);
+
+    margin-bottom: 2rem;
+  }
+
+  p{
+    font-size: 1.4rem;
+    color: var(--sub-text);
+
+    margin-bottom: 2rem;
+  }
+
+  div{
+    input{
+      font-size: 1.1rem;
+      width: 90%;
+      height: 4rem;
+      padding: 2rem 1rem;
+      z-index: 100;
+      box-shadow: 0px 0px 40px rgba(0, 0, 0, 0.6);
+      border: none;
+    }
+  }
+
+  i{
+      font-size: 2rem;
+      color: var(--sub-text);
+      position: relative;
+      right: -9rem;
+      top: -3rem;
+    }
+  img{
+    position: relative;
+    width: 100%;
+    margin-top: -4.5rem;
+    z-index: -100;
+  }
+`

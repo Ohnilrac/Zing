1d312e17dd990289798aea67760747f6689ce347
Jonas Nunes <jonasctps@gmail.com>
2022-07-29 15:06:53 -0300

    Products 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { About } from "./components/about";
 import { Header } from "./components/header";
 import { Home } from "./components/home";
+import { Products } from "./components/products";
 import { GlobalStyles } from "./styles/global";
 
 export function App() {
@@ -9,6 +10,7 @@ export function App() {
       <Header />
       <Home />
       <About/>
+      <Products />
       <GlobalStyles />
     </div>
   )
---
+++ src/components/products/index.tsx
@@ -0,0 +1,72 @@
+import { Container } from "./styles";
+
+export function Products(){
+  return(
+    <Container>
+      <div>
+        <h2>Produtos</h2>
+        <ul>
+          <li>Todos</li>
+          <li>Cadeiras</li>
+          <li>Sofá</li>
+          <li>Mesas</li>
+          <li>Camas</li>
+          <li>Armários</li>
+          <li>Gabinete</li>
+        </ul>
+      </div>
+      <div>
+        <section>
+          <article>
+            <img src="" alt="" />
+            <h4>Banco</h4>
+            <span>200</span>
+            <i></i>
+          </article>
+          <article>
+            <img src="" alt="" />
+            <h4>Mesa de chá</h4>
+            <span>150</span>
+            <i></i>
+          </article>
+          <article>
+            <img src="" alt="" />
+            <h4>Sofá - Claro</h4>
+            <span>300</span>
+            <i></i>
+          </article>
+          <article>
+            <img src="" alt="" />
+            <h4>Sofá - Escuro</h4>
+            <span>300</span>
+            <i></i>
+          </article>
+          <article>
+            <img src="" alt="" />
+            <h4>Lâmpada de mesa</h4>
+            <span>50</span>
+            <i></i>
+          </article>
+          <article>
+            <img src="" alt="" />
+            <h4>Cadeira</h4>
+            <span></span>
+            <i>200</i>
+          </article>
+          <article>
+            <img src="" alt="" />
+            <h4>Mesa de cabeceira</h4>
+            <span>100</span>
+            <i></i>
+          </article>
+          <article>
+            <img src="" alt="" />
+            <h4>Cadeira - Clara</h4>
+            <span>80</span>
+            <i></i>
+          </article>
+        </section>
+      </div>
+    </Container>
+  )
+}

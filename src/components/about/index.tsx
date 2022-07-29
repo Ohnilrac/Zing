c40d046c49566a93d5246d17f4fe84bc3529db86
Jonas Nunes <jonasctps@gmail.com>
2022-07-29 14:30:18 -0300

    About 1.0
    Estilizacao do about

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -1,50 +1,53 @@
-import { Container, Content } from './styles'
+import { Container, Content, SectionContainer, TittleSection } from './styles'
+import aboutImg from '../../assets/img-about.png'
 
 export function About() {
   return (
     <Container>
       <Content>
         <div>
-          <h2>Sobre nós</h2>
-          <p>
-            O conforto é fundamental e como você se sente sobre sua cadeira
-            afetará como você se sente e seu humor.
-          </p>
+          <TittleSection>
+            <h2>Sobre nós</h2>
+            <p>
+              O conforto é fundamental e como você se sente sobre sua cadeira
+              afetará como você se sente e seu humor.
+            </p>
+          </TittleSection>
 
-          <section>
-            <article>
-              <span>1</span>
-              <strong>Missão</strong>
-              <p>
-                Deixar as pessoas com conforto e que elas sintam-se em um
-                ambiente tranquilizador.
-              </p>
-            </article>
-            <article>
-              <span>2</span>
-              <strong>Motivação</strong>
-              <p>Uma complementação perfeita para o quarto ou sala.</p>
-            </article>
-            <article>
-              <span>3</span>
-              <strong>Visão</strong>
-              <p>
-                Juntos, dar uma dose de uma visão e espaço saudável para os
-                olhos que observam.
-              </p>
-            </article>
-            <article>
-              <span>4</span>
-              <strong>Meta</strong>
-              <p>
-                Ter significado para as pessoas que vivem no espaço em que
-                habitam nossas artes.
-              </p>
-            </article>
-          </section>
+            <SectionContainer>
+              <article>
+                <span>1</span>
+                <strong>Missão</strong>
+                <p>
+                  Deixar as pessoas com conforto e que elas sintam-se em um
+                  ambiente tranquilizador.
+                </p>
+              </article>
+              <article>
+                <span>2</span>
+                <strong>Motivação</strong>
+                <p>Uma complementação perfeita para o quarto ou sala. Tudo para acomodação do cliente.</p>
+              </article>
+              <article>
+                <span>3</span>
+                <strong>Visão</strong>
+                <p>
+                  Juntos, dar uma dose de uma visão e espaço saudável para os
+                  olhos que observam.
+                </p>
+              </article>
+              <article>
+                <span>4</span>
+                <strong>Meta</strong>
+                <p>
+                  Ter significado para as pessoas que vivem no espaço em que
+                  habitam nossas artes.
+                </p>
+              </article>
+            </SectionContainer>
         </div>
         <div>
-          <img src="" alt="" />
+          <img src={aboutImg} alt="Quato imagens de partes de uma sala decorada com cadeiras, poltronas, jarros e etc." />
         </div>
       </Content>
     </Container>
--- src/components/about/styles.ts
+++ src/components/about/styles.ts
@@ -1,9 +1,57 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const Container = styled.div`
   width: 80%;
-  margin: 0 auto;
+  margin: 3rem auto;
 `
 export const Content = styled.div`
   width: 100%;
+
+  img{
+    width: 100%;
+    display: none;
+  }
+`
+export const TittleSection = styled.div`
+  text-align: center;
+
+  h2{
+    font-size: 3rem;
+    font-weight: 700;
+    color: var(--text);
+    margin-bottom: 1rem;
+  }
+
+  p{
+    font-size: 1.4rem;
+    color: var(--sub-text);
+    margin-bottom: 3rem;
+  }
+`
+
+export const SectionContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 2.5rem;
+
+  article{
+    display: grid;
+    gap: 0.5rem;
+
+    span{
+      font-size: 3rem;
+      font-weight: 700;
+      color: var(--orange);
+    }
+
+    strong{
+      font-size: 1.5rem;
+      color: #000000;
+    }
+
+    p{
+      font-size: 1.4rem;
+      color: var(--sub-text);
+    }
+  }
 `
